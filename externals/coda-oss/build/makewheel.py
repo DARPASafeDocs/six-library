fdb6809f4a363645818d412243c2c38560b1eaf7
JonathanMeans <jonathan.means@mdaus.com>
2019-04-05 09:56:11 -0400

    More debugging

--- externals/coda-oss/build/makewheel.py
+++ externals/coda-oss/build/makewheel.py
@@ -26,6 +26,7 @@ class makewheel(BuildContext):
         print('Contents: {}'.format(os.listdir(os.getcwd())))
         print('Install contents: {}'.format(os.listdir(os.path.join(os.getcwd(),
                                                                     binDir.split('\\')[0]))))
+        print('Bin contents: {}'.format(oslistdir(binDir)))
         print('Attempting to call {}'.format(command))
         subprocess.call(command.split())
         os.remove('setup.py')
